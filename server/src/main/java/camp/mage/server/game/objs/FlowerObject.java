39d16a8d9b0cb3ec9def76b2805b93faf3d32d2d
Jacob Ferrero <jacobaferrero@gmail.com>
2018-01-04 20:37:49 -0600

    * Add Flower * Add Flower Spawn Area * Add ability to hide/show objects * Make "editor only" objects * Break out object  position to base object * Add editor enabled/disabled obserable

---
+++ server/src/main/java/camp/mage/server/game/objs/FlowerObject.java
@@ -0,0 +1,13 @@
+package camp.mage.server.game.objs;
+
+import camp.mage.server.game.World;
+
+/**
+ * Created by jacob on 1/4/18.
+ */
+
+public class FlowerObject extends BaseObject {
+    public FlowerObject(World world) {
+        super(world);
+    }
+}
---
+++ server/src/main/java/camp/mage/server/game/objs/FlowerSpawnAreaObject.java
@@ -0,0 +1,8 @@
+package camp.mage.server.game.objs;
+
+/**
+ * Created by jacob on 1/4/18.
+ */
+
+public class FlowerSpawnAreaObject {
+}
