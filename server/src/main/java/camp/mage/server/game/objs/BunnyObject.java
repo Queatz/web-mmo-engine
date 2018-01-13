e7f75ba147f5865d494c47f117a3339bb5d8b391
Jacob Ferrero <jacobaferrero@gmail.com>
2018-01-13 16:02:48 -0600

    Add bunnies

---
+++ server/src/main/java/camp/mage/server/game/objs/BunnyObject.java
@@ -0,0 +1,8 @@
+package camp.mage.server.game.objs;
+
+/**
+ * Created by jacob on 1/13/18.
+ */
+
+public class BunnyObject extends CharacterObject {
+}
---
+++ server/src/main/java/camp/mage/server/game/objs/behaviors/RunAroundBehavior.java
@@ -0,0 +1,8 @@
+package camp.mage.server.game.objs.behaviors;
+
+/**
+ * Created by jacob on 1/13/18.
+ */
+
+public class RunAroundBehavior {
+}
